579744ec023201880930c5933018e3649b2d0f75
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2023-07-13 14:25:37 +0700

    Implement SessionManager and AuthManager

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -7,15 +7,48 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Authentication() gin.HandlerFunc {
+type AuthMiddleware interface {
+	RequireAuthentication() gin.HandlerFunc
+}
+
+type authMiddleware struct {
+	sessionManager SessionManager
+}
+
+type SessionManager interface {
+	GetCurrentUsername(c *gin.Context) (string, bool)
+}
+
+type sessionManager struct {
+}
+
+func (s *sessionManager) GetCurrentUsername(c *gin.Context) (string, bool) {
+	session := sessions.Default(c)
+	username, exists := session.Get("username").(string)
+	return username, exists
+}
+
+func NewAuthMiddleware(sessionManager SessionManager) AuthMiddleware {
+	return &authMiddleware{
+		sessionManager: sessionManager,
+	}
+}
+
+func (a *authMiddleware) RequireAuthentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		session := sessions.DefaultMany(c, "user_session")
-		username := session.Get("username")
-		if username == nil {
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": "unauthorized",
+		username, exists := a.sessionManager.GetCurrentUsername(c)
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
 			})
 			c.Abort()
+			return
 		}
+		c.Set("username", username)
+		c.Next()
 	}
+}
+
+func NewSessionManager() SessionManager {
+	return &sessionManager{}
 }
